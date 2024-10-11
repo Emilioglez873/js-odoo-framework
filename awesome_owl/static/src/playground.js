247f20319205a8a3ab8334bf6087c02ffc39efda
Emilio <emiliog@vauxoo.com>
2024-10-11 16:44:48 -0600

    [ADD] Awesome Owl: 1.6 The sum of two `Counter`

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, markup } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
@@ -8,5 +8,13 @@ export class Playground extends Component {
     static template = "awesome_owl.playground";
 
     innerHtml = markup("<div>some text 2</div>");
+
+    setup(){
+        this.sum = useState({ value: 2 });
+    }
+    incrementSum() {
+        this.sum.value++;
+    }
+
     static components = { Counter, Card };
 }
