f87a23441d6bac23f1a3772611a98117b2362af7
Emilio <emiliog@vauxoo.com>
2024-10-11 16:33:28 -0600

    [ADD] Awesome Owl: 1.4 Using `markup` to display html

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,12 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+
+    innerHtml = markup("<div>some text 2</div>");
     static components = { Counter, Card };
 }
