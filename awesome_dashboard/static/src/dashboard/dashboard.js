53fb44e7218f3c7c1bb14d20e5659dcc7c6db2f0
Emilio <emiliog@vauxoo.com>
2024-10-17 07:08:33 -0600

    [ADD] Awesome Dashboard: 2.11  Add and remove dashboard items

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -1,12 +1,14 @@
 /** @odoo-module **/
 
-import { Component, onWillStart, useState } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from '@web/search/layout';
 import { useService } from "@web/core/utils/hooks";
 import { DashboardItem} from "./dashboard_item/dashboard_item"
 import { PieChart } from "./pie_chart/pie_chart";
-import { items } from "./dashboard_items";
+import { Dialog } from "@web/core/dialog/dialog";
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { browser } from "@web/core/browser/browser";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
@@ -14,11 +16,15 @@ class AwesomeDashboard extends Component {
     static components = { Layout, DashboardItem, PieChart };
     setup() {
         this.action = useService("action");
+        this.dialog = useService("dialog");
         this.statistics = useState(useService("awesome_dashboard.statistics"));
         this.display = {
             controlPanel: {},
         };
         this.items = registry.category("awesome_dashboard").getAll();
+        this.state = useState({
+            disabledItems: browser.localStorage.getItem("disabledDashboardItems")?.split(",") || []
+        });
     }
     openKanbanView() {
         this.action.doAction("base.action_partner_form");
@@ -36,7 +42,45 @@ class AwesomeDashboard extends Component {
             ],
         });
     }
+    openConfiguration(){
+        this.dialog.add(ConfigurationDialog, {
+            items: this.items,
+            disabledItems: this.state.disabledItems,
+            onUpdateConfiguration: this.updateConfiguration.bind(this),
+        })
+    }
+    updateConfiguration(newDisabledItems) {
+        this.state.disabledItems = newDisabledItems;
+    }
 
+}
+
+class ConfigurationDialog extends Component {
+    static template = "awesome_dashboard.ConfigurationDialog";
+    static components = { Dialog, CheckBox };
+    static props = ["close", "items", "disabledItems", "onUpdateConfiguration"];
+    setup() {
+        this.items = useState(this.props.items.map((item) => {
+            return {
+                ...item,
+                enabled: !this.props.disabledItems.includes(item.id),
+            }
+        }));
+    }
+    done() {
+        this.props.close();
+    }
+    onChange(checked, changedItem) {
+        changedItem.enabled = checked;
+        const newDisabledItems = Object.values(this.items).filter(
+            (item) => !item.enabled
+        ).map((item) => item.id)
+        browser.localStorage.setItem(
+            "disabledDashboardItems",
+            newDisabledItems,
+        );
+        this.props.onUpdateConfiguration(newDisabledItems);
+    }
 }
 
 registry.category("lazy_components").add("AwesomeDashboard", AwesomeDashboard);
