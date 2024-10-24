adff33dca489b0c95973bc14455e8294b5cc336c
Emilioglez873 <guimelemilio@gmai.com>
2024-10-24 09:44:40 -0600

    [ADD] Awesome dashboard: Adding a comment in statistics service

--- awesome_dashboard/static/src/dashboard/statistics_service.js
+++ awesome_dashboard/static/src/dashboard/statistics_service.js
@@ -7,6 +7,8 @@ const statisticsService = {
     dependencies: ["rpc"],
     start(env, {rpc}) {
         const statistics = reactive({isReady: false});
+        // This part was following the instructions in the 7th step of the Chapter 2 of Discover web framework in which the service
+        // should return a reactive object that is being updated every 10 minutes
         async function loadData() {
             const updates = await rpc("/dashboard/statistics");
             Object.assign(statistics, updates, {isReady: true});
